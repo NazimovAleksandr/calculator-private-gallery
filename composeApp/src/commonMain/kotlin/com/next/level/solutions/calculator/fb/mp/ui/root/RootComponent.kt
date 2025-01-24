e33cc3a1cc7cafad055e320790e2f1f3a18bdec6
Aleksandr Nazimov <aleksandr.nazimov@weadmire.io>
2025-01-24 18:07:55 +0500

    Refactor: Integrate AdsManager into RootComponent.Child

--- composeApp/src/commonMain/kotlin/com/next/level/solutions/calculator/fb/mp/ui/root/RootComponent.kt
+++ composeApp/src/commonMain/kotlin/com/next/level/solutions/calculator/fb/mp/ui/root/RootComponent.kt
@@ -1,12 +1,17 @@
 package com.next.level.solutions.calculator.fb.mp.ui.root
 
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import com.next.level.solutions.calculator.fb.mp.ecosystem.ads.AdsManager
+import com.next.level.solutions.calculator.fb.mp.ecosystem.ads.nativ.NativeSize
 import com.next.level.solutions.calculator.fb.mp.utils.KoinFactory
 import kotlinx.serialization.Serializable
 
@@ -36,15 +41,34 @@ class RootComponent(
   private fun Configuration.toChild(
     componentContext: ComponentContext,
   ): Child {
-    componentContext.instanceKeeper.put(instanceKey, instanceKeeper())
+    componentContext.instanceKeeper.put(componentContext, instanceKeeper())
     return with(this) { factory.get(componentContext) }
   }
 
   /**
    * Component contract - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    */
-  interface Child {
-    fun content(): @Composable () -> Unit
+  abstract class Child(
+    private val adsManager: AdsManager,
+  ) {
+    interface Action
+
+    abstract fun content(): @Composable () -> Unit
+    abstract fun action(action: Action)
+
+    fun nativeAdCard(
+      size: NativeSize?,
+      modifier: Modifier = Modifier,
+      loadAtDispose: Boolean = true,
+      color: Color? = null,
+    ): @Composable ColumnScope.() -> Unit = {
+      adsManager.native.ad(
+        size = size,
+        modifier = modifier,
+        loadAtDispose = loadAtDispose,
+        color = color,
+      ).invoke(this)
+    }
   }
 
   @Serializable
