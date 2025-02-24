ce5e1c489deeaca6847b0cfa19a717704772f7e8
Aleksandr Nazimov <aleksandr.nazimov@weadmire.io>
2025-02-24 22:30:09 +0500

    Android: SplashScreen API

--- composeApp/src/androidMain/kotlin/com/next/level/solutions/calculator/fb/mp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/next/level/solutions/calculator/fb/mp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arkivanov.decompose.defaultComponentContext
 import com.next.level.solutions.calculator.fb.mp.ecosystem.ads.AdsManager
 import com.next.level.solutions.calculator.fb.mp.ecosystem.ads.AdsManagerImpl
@@ -24,7 +25,13 @@ class MainActivity : ComponentActivity() {
     var adsManager: AdsManager? = null
     var languageChanger: Lazy<LanguageChangerImpl>? = null
     var producePath: ((String) -> String)? = null
+    var externalStoragePermissionGranted: (() -> Boolean)? = null
+    var requestExternalStoragePermission: (() -> Unit)? = null
+    var collapse: (() -> Unit)? = null
+    var systemBars: ((Boolean) -> Unit)? = null
+    var appEventListeners: ((AppEvent) -> Unit)? = null
     var roomDatabase: (() -> RoomDatabase.Builder<MyDatabase>)? = null
+    var fileHider: (() -> FileHider)? = null
   }
 
   override fun attachBaseContext(newBase: Context?) {
@@ -47,6 +54,7 @@ class MainActivity : ComponentActivity() {
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
 
     producePath = { filesDir.resolve(it).absolutePath }
@@ -56,6 +64,52 @@ class MainActivity : ComponentActivity() {
         activity = this,
         store = ChangerLocalStore(getSharedPreferences("Changer", Context.MODE_PRIVATE))
       )
+    }
+
+    requestExternalStoragePermission = {
+      if (Build.VERSION.SDK_INT >= 30) {
+        try {
+          val intent = Intent(
+            Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+            Uri.parse("package:$packageName"),
+          )
+
+          intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+
+          startActivity(intent)
+        } catch (e: Exception) {
+          val intent = Intent(
+            Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION,
+          )
+          intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+
+          startActivity(intent)
+        }
+      } else {
+        launcher.launch(
+          arrayOf(
+            READ_EXTERNAL_STORAGE,
+            WRITE_EXTERNAL_STORAGE,
+          )
+        )
+      }
+    }
+
+    systemBars = {
+      systemBars(it)
+    }
+
+    collapse = {
+      startActivity(
+        Intent(Intent.ACTION_MAIN).apply {
+          addCategory(Intent.CATEGORY_HOME)
+          flags = FLAG_ACTIVITY_NEW_TASK
+        }
+      )
+    }
+
+    fileHider = {
+      FileHider(this)
     }
 
     roomDatabase = {
